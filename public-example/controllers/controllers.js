1238f8ad2848feaca20c451853e35e8f3ffde7bb
Span <etradeon@gmail.com>
2019-05-21 23:08:53 +0100

    changes

--- public-example/controllers/controllers.js
+++ public-example/controllers/controllers.js
@@ -1,9 +1,16 @@
 
-const config = require('../../config.js')
+const config = require('../../core/api/config.js')
 const axios = require('axios')
 
 function getPoll(req, res){
-    axios.get(`${config.apiPath}/poll/Favourite-letter`)
+    axios.get(`${config.apiPath}/poll/Favourite-letter`, {
+        headers: {
+            'Authorization': 'fsad213asd5435',
+        },
+        data : {
+            'secret': 'Fjasdoks1909asd'
+        }
+    })
     .then((result) => {
         res.render('index.ejs', {data: result.data})
     })
